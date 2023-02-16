798cfc8ed01232f39c53f3fcf59c00367e2f4e89
Miguel Angel Sergio Ramirez Medel <miguelangel@MacBook-Pro-de-Miguel.local>
2023-02-16 17:55:41 -0600

    cambios

--- InputValue/view/frontend/web/js/input_value.js
+++ InputValue/view/frontend/web/js/input_value.js
@@ -9,27 +9,24 @@ define([
         defaults: {
             //template: 'ChatGpt_InputValue/input_value'
         },
+
+        inputValue: ko.observable(''),
+        displayValue: ko.computed(function() {
+            return 'El valor ingresado es: ' + this.inputValue();
+        }),
+        
         initialize: function () {
             this._super();
-            var self = this;
-            this.inputValue = ko.observable('');
-            this.displayValue = ko.computed(function() {
-                return 'El valor ingresado es: ' + self.inputValue();
-            });
             this.getInputValue();
         },
 
-        getInputValue: function(){
-            var self = this;
+        getInputValue: function() {
             $('#input_component').on('change', function() {
-                console.log('entraa');
-                self.inputValue(this.value);
+              this.inputValue(this.value);
             });
-            return function (config, element) {
-                ko.applyBindings({
-                    displayValue: self.displayValue
-                }, element);
-            };
-        }
+            ko.applyBindings({
+              displayValue: this.displayValue
+            }, document.getElementById('value_component'));
+          }
     });
 });
