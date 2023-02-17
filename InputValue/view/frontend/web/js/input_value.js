b6b215d25d0229d002cd96b5731a4112a23f2d37
Miguel Angel Sergio Ramirez Medel <miguelangel@MacBook-Pro-de-Miguel.local>
2023-02-16 19:35:12 -0600

    Agregamos JSdoc

--- InputValue/view/frontend/web/js/input_value.js
+++ InputValue/view/frontend/web/js/input_value.js
@@ -1,3 +1,10 @@
+/**
+ * Este es un componente de Magento que muestra el valor ingresado en un input y lo muestra en pantalla.
+ *
+ * @module ChatGpt_InputValue/input_value
+ * @see {@link https://github.com/miikEe13/InputValue}
+ * @exports ChatGpt_InputValue/js/input_value
+ */
 define([
     'jquery',
     'uiComponent',
@@ -5,17 +12,44 @@ define([
 ], function ($, Component, ko) {
     'use strict';
 
+    /**
+     * El componente de input.
+     *
+     * @class
+     * @extends uiComponent
+     * @param {Object} config - Configuración del componente
+     * @param {string} config.template - Plantilla del componente
+     * @param {Object} element - Elemento del DOM del componente
+     */
     return Component.extend({
         defaults: {
             template: 'ChatGpt_InputValue/input_value'
         },
-        inputValue: ko.observable(''),
-        displayValue: ko.observable(''),
 
+        /**
+         * @type {Observable<string>}
+         */
+        inputValue: ko.observable(''), // Almacena el valor del input
+
+        /**
+         * @type {Observable<string>}
+         */
+        displayValue: ko.observable(''), // Almacena el valor que se muestra en pantalla
+
+        /**
+         * Función que se ejecuta cuando cambia el valor del input.
+         * 
+         * @function getInputValue
+         */
         getInputValue: function () {
             this.displayValue('El valor ingresado es: ' + this.inputValue());
         },
 
+        /**
+         * Función que se ejecuta al inicializar el componente.
+         * 
+         * @function initialize
+         */
         initialize: function () {
             this._super();
         }
