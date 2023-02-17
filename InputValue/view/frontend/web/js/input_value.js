01af3a6332b63e9fd7275e99d8da0637c36d1f60
Miguel Angel Sergio Ramirez Medel <miguelangel@MacBook-Pro-de-Miguel.local>
2023-02-16 18:40:54 -0600

    more changes

--- InputValue/view/frontend/web/js/input_value.js
+++ InputValue/view/frontend/web/js/input_value.js
@@ -1,33 +1,28 @@
 define([
-    'jquery',
-    'uiComponent',
+    'jquery', 
+    'uiComponent', 
     'ko'
 ], function ($, Component, ko) {
     'use strict';
 
     return Component.extend({
         defaults: {
+            template: 'Chat_InputValue/input_value'
         },
-
         inputValue: ko.observable(''),
-
         displayValue: ko.computed(function() {
             return 'El valor ingresado es: ' + this.inputValue();
         }),
 
+        getInputValue: function() {
+            this.inputValue($('#input_component').val());
+        },
+
         initialize: function () {
             this._super();
             var input = $('#input_component');
-            input.change(this.getInputValue());
+            input.change(this.getInputValue.bind(this));
             ko.applyBindings(this, input[0]);
-        },
-
-        getInputValue: function() {
-            this.inputValue($('#input_component').val());
-            var self = this;
-            return function () {
-                self.inputValue(this.value);
-            };
-        },
+        }
     });
 });
